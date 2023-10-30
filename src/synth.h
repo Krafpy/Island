81a4ca6be29e5b123c51560fae31a32c52709815
Krafpy <nazar.misyats@gmail.com>
2023-10-30 02:08:21 +0100

    Simplified defines

--- src/synth.h
+++ src/synth.h
@@ -4,20 +4,18 @@
 // by 2pi. This reduces the size of the code, a bit.
 #define FREQ_AS_PULSE
 
-#define _OCT(oct) (12*(oct))
-
-#define C(oct)  ((char)(_OCT(oct) + 0))
-#define Cd(oct) ((char)(_OCT(oct) + 1))
-#define D(oct)  ((char)(_OCT(oct) + 2))
-#define Dd(oct) ((char)(_OCT(oct) + 3))
-#define E(oct)  ((char)(_OCT(oct) + 4))
-#define F(oct)  ((char)(_OCT(oct) + 5))
-#define Fd(oct) ((char)(_OCT(oct) + 6))
-#define G(oct)  ((char)(_OCT(oct) + 7))
-#define Gd(oct) ((char)(_OCT(oct) + 8))
-#define A(oct)  ((char)(_OCT(oct) + 9))
-#define Ad(oct) ((char)(_OCT(oct) + 10))
-#define B(oct)  ((char)(_OCT(oct) + 11))
+#define C(oct)  ((char)(12*(oct) + 0))
+#define Cd(oct) ((char)(12*(oct) + 1))
+#define D(oct)  ((char)(12*(oct) + 2))
+#define Dd(oct) ((char)(12*(oct) + 3))
+#define E(oct)  ((char)(12*(oct) + 4))
+#define F(oct)  ((char)(12*(oct) + 5))
+#define Fd(oct) ((char)(12*(oct) + 6))
+#define G(oct)  ((char)(12*(oct) + 7))
+#define Gd(oct) ((char)(12*(oct) + 8))
+#define A(oct)  ((char)(12*(oct) + 9))
+#define Ad(oct) ((char)(12*(oct) + 10))
+#define B(oct)  ((char)(12*(oct) + 11))
 
 
 #define TWO_PI 6.28318530718f
