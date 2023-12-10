e2b87f4116e9ebc49e1fc812839f27b6dcc4d66f
Krafpy <nazar.misyats@gmail.com>
2023-12-10 03:58:38 +0100

    Added release resolution

--- src/config.h
+++ src/config.h
@@ -5,7 +5,12 @@
 
 #define INTRO_DURATION 5 // seconds
 
+#ifdef DEBUG
 #define X_RES 640
 #define Y_RES 480
+#else
+#define X_RES 1280
+#define Y_RES 720
+#endif
 
 #define MUSIC_BPM (60*20/INTRO_DURATION)
