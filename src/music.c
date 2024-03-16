aaf3f1b3dbc8b162b6540da29626d9538d786e1c
Krafpy <nazar.misyats@gmail.com>
2024-03-16 14:55:15 +0100

    First audio attempt

--- src/music.c
+++ src/music.c
@@ -4,11 +4,27 @@
 #include "fp.h"
 #include <math.h>
 
-#define MUSIC_BPM (60*20/INTRO_DURATION)
-#define BEAT_DURATION (60.f/(float)MUSIC_BPM)
+#define N_NOTES(notes) (sizeof(notes)/sizeof(char))
+
+#define BPM 10
+#define BDUR 5.f //(60.f/(float)BPM)
 #define MAX_AMPLITUDE (32767.0f/4.f) // must be less or equal than 32767
 
-static char notes[] = {C(3), G(3), F(3), G(3)};
+static char notes[] = {C(2), G(2), F(2), G(2)};
+
+static float music_wave(float t) {
+    float sig = 0.f;
+
+    float fc = sequence(t, notes, N_NOTES(notes), BDUR);
+    sig += 0.5*osc_tri(fc*t + 1.f*sinf((fc-1.f)*t));
+    sig += sinf((fc+2.f)*t + 1.5f*sinf((fc+1.f)*t));
+    sig *= 0.25f;
+
+    float nt = fmodf(t, BDUR);
+    sig *= envelope(nt, 0.1f*BDUR, 0.85f*BDUR, 0.05f*BDUR);
+
+    return sig;
+}
 
 #define N_NOTES (sizeof(notes)/sizeof(char))
 
@@ -16,11 +32,11 @@ void music_init(short* buffer) {
     // Generate the music's audio signal
     for(unsigned int i = 0; i < NUM_SAMPLES; i++) {
         float t = (float)i/(float)SAMPLE_RATE;
-        float u = t/BEAT_DURATION;
-
-        char n = notes[(int)(t/BEAT_DURATION) % N_NOTES];
-        float f = freq(n);
-        float v = fmodf(u, 1.f);
+        
+        float sig = music_wave(t);
+        float rev = music_wave(t-0.5f);
+        sig += 0.25f*rev;
+        sig *= 0.3f;
 
         float out = tri(f*t);
         out *= envelope(v, 0.1f, 0.1f, 0.5f);
